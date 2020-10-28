b9fe678578f417ef7175f9e09d1a4d5ad53896f8
aosucas499 <aosucas499@gmail.com>
2020-10-29 00:26:49 +0100

    Update v.0.6

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 #screen -d -m bash /usr/sbin/nwfermi_daemon.sh
 #rm -f /var/run/dbus/pid > /dev/null 2>&1
 #sleep 1
-#export LANG=es_ES.UTF-8
+export LANG=es_ES.UTF-8
 screen -d -m /opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMARTBoardService_elf &
 /opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMART\ Board\ Tools_elf
 
