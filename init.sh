5dc6b282e91998982083ac50990cccada782b6fb
aosucas499 <aosucas499@gmail.com>
2020-10-31 14:53:18 +0100

    nwfermi in user session

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-#screen -d -m bash /usr/sbin/nwfermi_daemon.sh
-#rm -f /var/run/dbus/pid > /dev/null 2>&1
-#sleep 1
+screen -d -m bash /usr/sbin/nwfermi_daemon.sh
+rm -f /var/run/dbus/pid > /dev/null 2>&1
+sleep 1
 export LANG=es_ES.UTF-8
 screen -d -m /opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMARTBoardService_elf &
 /opt/SMART\ Technologies/SMART\ Product\ Drivers/bin/.SMART\ Board\ Tools_elf
